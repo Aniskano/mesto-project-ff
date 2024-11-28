5108809da66255e628a375b964f156063c94cd77
Anna Novikova <anyano@mail.ru>
2024-11-28 12:22:03 +0300

    first iteration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Aniskano/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const template = document
+  .querySelector('#card-template')
+  .content.querySelector('.card');
 
-// @todo: DOM узлы
+// DOM узлы
+const container = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(item, { deleteCard }) {
+  const cardElementCopy = template.cloneNode(true),
+    cardTitle = cardElementCopy.querySelector('.card__title'),
+    cardImage = cardElementCopy.querySelector('.card__image'),
+    deleteButton = cardElementCopy.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = item.link;
+  cardTitle.textContent = item.name;
+  cardImage.alt = item.name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => deleteCard(cardElementCopy));
+
+  return cardElementCopy;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+function addCard(card) {
+  const cardToAdd = createCard(card, { deleteCard });
+  container.prepend(cardToAdd);
+}
+
+initialCards.forEach(addCard);
